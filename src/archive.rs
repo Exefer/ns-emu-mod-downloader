91eb6b104da005e6848b5637f0c4d28ddebd1400
Exefer <jessieodeh08@gmail.com>
2025-08-09 21:46:24 +0200

    refactor: switch to sevenz-rust util function for archive extraction

--- src/archive.rs
+++ src/archive.rs
@@ -1,31 +1,9 @@
-use sevenz_rust::SevenZReader;
-use std::fs;
 use std::path::Path;
 
 pub fn extract_archive<P: AsRef<Path>>(
     archive_path: P,
     output_dir: P,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let mut sz = SevenZReader::open(archive_path, sevenz_rust::Password::empty())?;
-
-    fs::create_dir_all(&output_dir)?;
-
-    sz.for_each_entries(|entry, reader| {
-        if !entry.is_directory() {
-            let output_path = output_dir.as_ref().join(&entry.name);
-
-            if let Some(parent) = output_path.parent() {
-                fs::create_dir_all(parent)?;
-            }
-
-            let mut output_file = fs::File::create(&output_path)?;
-            std::io::copy(reader, &mut output_file)?;
-        } else {
-            let dir_path = output_dir.as_ref().join(&entry.name);
-            fs::create_dir_all(dir_path)?;
-        }
-        Ok(true)
-    })?;
-
-    Ok(())
+    sevenz_rust::decompress_file(archive_path, output_dir)
+        .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
 }
