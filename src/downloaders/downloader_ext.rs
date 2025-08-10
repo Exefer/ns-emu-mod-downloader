352c1356154f6e182a87faca3dcd2529d6ed5794
Exefer <jessieodeh08@gmail.com>
2025-08-10 11:15:14 +0200

    refactor: run `cargo clippy --fix`

--- src/downloaders/downloader_ext.rs
+++ src/downloaders/downloader_ext.rs
@@ -8,7 +8,7 @@ impl<M: super::mod_downloader::ModDownloader> TitleVersionExt for M {
     fn get_title_version(&self, title_id: &str) -> Result<String, Box<dyn std::error::Error>> {
         let default_version = "1.0.0";
         let mut pv_path = dirs::cache_dir().unwrap();
-        pv_path.push(&EMU_NAME.get().unwrap());
+        pv_path.push(EMU_NAME.get().unwrap());
         pv_path.push("game_list");
         pv_path.push(format!("{}.pv.txt", title_id));
 
